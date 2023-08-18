f833c4dcef543ab7980730888dfea2b7cfc2caa4
Salih Kertik <96303345+salihkertik@users.noreply.github.com>
2023-08-18 03:09:27 +0300

    Add files via upload

---
+++ Projects/SimpsonBook/SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,24 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Salih Kertik on 18.08.2023.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage){
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
