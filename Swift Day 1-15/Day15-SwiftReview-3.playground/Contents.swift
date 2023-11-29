62c0c83785b60cc8ddd2076e5e55f277ab058598
Salih Kertik <salihkertik1453@gmail.com>
2023-11-29 20:14:52 +0300

    Chapter Two

--- Swift Day 1-15/Day15-SwiftReview-3.playground/Contents.swift
+++ Swift Day 1-15/Day15-SwiftReview-3.playground/Contents.swift
@@ -1,16 +1,40 @@
 import UIKit
 // Creat day 15
 
+/*
+ // Chapter One
+ 
+ struct Person {
+     var clothes: String
+     var shoes: String
+     
+     func describe(){
+         print("I like wearing \(clothes) with \(shoes)")
+     }
+ }
+
+ let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+ let other = Person(clothes: "short skirts", shoes: "high heels")
+ taylor.describe()
+ other.describe()
+ */
+
+// Chapter Two
+
 struct Person {
-    var clothes: String
-    var shoes: String
-    
-    func describe(){
-        print("I like wearing \(clothes) with \(shoes)")
+    var clothes: String{
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+        didSet{
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
     }
 }
 
-let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
-let other = Person(clothes: "short skirts", shoes: "high heels")
-taylor.describe()
-other.describe()
+func updateUI(msg: String){
+    print(msg)
+}
+
+var taylor = Person(clothes: "T-shirts")
+taylor.clothes = "short skirts"
