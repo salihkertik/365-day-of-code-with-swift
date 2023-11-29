79bbd9c21578b1e3f6ba1a5308bdf3ead37b7851
Salih Kertik <salihkertik1453@gmail.com>
2023-11-29 20:08:47 +0300

    Chatper One

--- Swift Day 1-15/Day15-SwiftReview-3.playground/Contents.swift
+++ Swift Day 1-15/Day15-SwiftReview-3.playground/Contents.swift
@@ -1,2 +1,16 @@
 import UIKit
 // Creat day 15
+
+struct Person {
+    var clothes: String
+    var shoes: String
+    
+    func describe(){
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+taylor.describe()
+other.describe()
