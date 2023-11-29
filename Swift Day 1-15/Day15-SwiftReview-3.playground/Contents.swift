f53a4b351ae5e648fed4b92300d8953b03b0d542
Salih Kertik <salihkertik1453@gmail.com>
2023-11-29 20:19:57 +0300

    Chapter Three
    Day 15 review completed

--- Swift Day 1-15/Day15-SwiftReview-3.playground/Contents.swift
+++ Swift Day 1-15/Day15-SwiftReview-3.playground/Contents.swift
@@ -1,40 +1,55 @@
 import UIKit
 // Creat day 15
 
-/*
+
  // Chapter One
  
  struct Person {
-     var clothes: String
-     var shoes: String
-     
-     func describe(){
-         print("I like wearing \(clothes) with \(shoes)")
-     }
+ var clothes: String
+ var shoes: String
+ 
+ func describe(){
+ print("I like wearing \(clothes) with \(shoes)")
  }
-
+ }
+ 
  let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
  let other = Person(clothes: "short skirts", shoes: "high heels")
  taylor.describe()
  other.describe()
- */
+ 
+ 
+ 
+ // Chapter Two
+ 
+ struct Person {
+ var clothes: String{
+ willSet {
+ updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+ }
+ didSet{
+ updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+ }
+ }
+ }
+ 
+ func updateUI(msg: String){
+ print(msg)
+ }
+ 
+ var taylor = Person(clothes: "T-shirts")
+ taylor.clothes = "short skirts"
+ 
 
-// Chapter Two
+// Chapter Three
 
 struct Person {
-    var clothes: String{
-        willSet {
-            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
-        }
-        didSet{
-            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
-        }
+    var age: Int
+    
+    var ageInDogYears: Int {
+        return age * 7
     }
 }
 
-func updateUI(msg: String){
-    print(msg)
-}
-
-var taylor = Person(clothes: "T-shirts")
-taylor.clothes = "short skirts"
+var fan = Person(age: 25)
+print(fan.ageInDogYears)
